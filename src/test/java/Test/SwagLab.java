c901290fa025380a620bb375ec81a83a4b778f7e
chaitali2493 <chaitali.cp24@gmail.com>
2023-10-27 12:40:02 +0530

    pom.xml modified by maven compiler

--- src/test/java/Test/SwagLab.java
+++ src/test/java/Test/SwagLab.java
@@ -29,6 +29,8 @@ public class SwagLab {
 	@Test
 	public void LoginTest()
 	{
+		
+		
 		driver.findElement(By.id("user-name")).sendKeys("standard_user");
 		driver.findElement(By.id("password")).sendKeys("secret_sauce");
 		driver.findElement(By.id("login-button")).click();
